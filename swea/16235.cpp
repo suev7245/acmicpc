69dc050a7c8fca273f274cfcc6a26d2bc9a502f3
suev7245 <suev7245@gmail.com>
2019-10-19 17:56:18 +0900

    solve fishman using queue to reduce time(timeout)

--- swea/16235.cpp
+++ swea/16235.cpp
@@ -1,123 +1,126 @@
 //나무 재테크
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
+struct Tree{
+    int y, x, z;
+    Tree(){}
+    Tree(int ty, int tx, int tz){
+        y=ty, x=tx, z=tz;
+    }
+};
 
-int N,M,K,dab;
+int N,M,K, dab;
 int A[10][10];
 int ground[10][10];
-vector<int> trees[10][10];
 
 int dy[]={-1,-1,-1,0,1,1,1,0};
 int dx[]={-1,0,1,1,1,0,-1,-1};
 
-void solve(){
-    while(K--){
-        int age;
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(trees[i][j].size()==0)continue;
-                sort(trees[i][j].begin(),trees[i][j].end());
-                // for(int k=0;k<trees[i][j].size();k++){
-                //     cout<<trees[i][j][k]<<" ";
-                // }cout<<"\n";
-            
-                //어린나무부터 양분 먹이기
-                int k, addtree=0;
-                for(k=0;k<trees[i][j].size();k++){
-                    age = trees[i][j][k];
-                    if(ground[i][j]>=age){
-                        ground[i][j]-=age;
-                        trees[i][j][k]++;//age++;
-                    }else{//k부터 양분 못 먹어서 죽어야 하는 나무 index- 여름 바로 처리
-                        break;
-                    }
-                }
-                if(k!=trees[i][j].size()){//여름처리
-                    for(int t=trees[i][j].size()-1;t>=k;t--){
-                        ground[i][j]+=trees[i][j][t]/2;
-                        trees[i][j].pop_back();
-                    }
-                }
-            }
-        }
-        
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(trees[i][j].size()==0)continue;
-                int addtree=0;
-                for(int k=0;k<trees[i][j].size();k++){
-                    if(trees[i][j][k]%5==0){//가을
-                        addtree++;
-                    }
-                }
-                int ny, nx;
-                for(int dir=0;dir<8 && addtree;dir++){//가을처리
-                    ny= i+dy[dir];
-                    nx= j+dx[dir];
-                    if(ny<0||ny>=N||nx<0||nx>=N) continue;
-                    for(int ii=0;ii<addtree;ii++)
-                        trees[ny][nx].push_back(1);
-                }
-                ground[i][j]+=A[i][j];
-            }
-        }
+queue<Tree> trees[2];
+queue<Tree> addtrees;
+queue<Tree> dead;
+int cur=0, nex=1;
 
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                cout<<trees[i][j].size()<<" ";
-            }cout<<"\n";
+bool comp(const Tree &a, const Tree &b){
+    return a.z<b.z;
+}
+
+void solve(){
+    //봄 - 번식한 애들은 나이 1이니까 얘네 먼저 처리 후, 있던 애들 처리.
+    Tree now;
+    while(!addtrees.empty()){
+        now = addtrees.front(); 
+        addtrees.pop();
+        // cout<<ground[now.y][now.x]<<","<<now.z<<" ";
+        if(ground[now.y][now.x]<now.z){
+            dead.push(now);//죽어
+        }else{
+            ground[now.y][now.x] -= now.z;//양분 먹고
+            now.z++;//나이 +1
+            trees[nex].push(now);
         }
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                for(int k=0;k<trees[i][j].size();k++){
-                    cout<<trees[i][j][k]<<",";
-                }
-            }
-        }cout<<"\n";
+    }
+    while(!trees[cur].empty()){
+        now= trees[cur].front();
+        trees[cur].pop();
+        // cout<<ground[now.y][now.x]<<","<<now.z<<" ";
+        if(ground[now.y][now.x]<now.z){
+            dead.push(now);//죽어
+        }else{
+            ground[now.y][now.x] -= now.z;//양분 먹고
+            now.z++;//나이 +1
+            trees[nex].push(now);
+        }
     }
 
+    //여름
+    while(!dead.empty()){
+        ground[dead.front().y][dead.front().x]+=dead.front().z/2;
+        dead.pop();
+    }
+
+    //가을
+    int ny, nx;
+    while(!trees[nex].empty()){
+        now= trees[nex].front(); trees[nex].pop();
+        trees[cur].push(now);
+        if(now.z % 5 != 0) continue;
+
+        for(int dir=0;dir<8;dir++){
+            ny= now.y + dy[dir];
+            nx= now.x + dx[dir];
+
+            if(ny<0||ny>=N||nx<0||nx>=N) continue;
+            addtrees.push(Tree(ny,nx,1));
+        }
+    }
+
+    //겨울
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
-            dab+=trees[i][j].size();
+            ground[i][j]+=A[i][j];
         }
     }
 }
+
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
     cin>>N>>M>>K;
+    //A배열 받기
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
             cin>>A[i][j];
-            ground[i][j]=5;
         }
     }
-
-    int a,b,c;
+    //나무 받기 - 처음 정렬한 후에는 무조건 +1과 최소 나이인 1 나무만 추가 되므로 순서가 망가지지 않음. 따라서 정렬은 한번만 해도 됨.
+    int x,y,z;
+    vector<Tree> temp;
     for(int i=0;i<M;i++){
-        cin>>a>>b>>c;
-        trees[a-1][b-1].push_back(c);
+        cin>>x>>y>>z;
+        temp.push_back(Tree(x-1,y-1,z));
+    }
+    sort(temp.begin(),temp.end(),comp);
+    for(int i=0;i<M;i++){
+        trees[0].push(temp[i]);
     }
 
-    solve();
+    //ground 배열 5로 초기화
+    for(int i=0;i<N;i++)
+        for(int j=0;j<N;j++)
+            ground[i][j]=5;
 
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            cout<<trees[i][j].size()<<" ";
-        }cout<<"\n";
+    while(K--){
+        solve();
+        // cout<<"cur:"<<trees[cur].size()<<" ";
+        // cout<<"nex:"<<trees[nex].size()<<" ";
     }
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            for(int k=0;k<trees[i][j].size();k++){
-                cout<<trees[i][j][k]<<" ";
-            }cout<<"\n";
-        }cout<<"\n";
-    }
-
-    cout<<dab;
+    cout<<trees[cur].size()+addtrees.size();
 } 
